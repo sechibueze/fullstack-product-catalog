4177e68611aa7bc0322ae575ee688c08603b9d64
Samuel Chibueze <sechibueze@gmail.com>
2026-04-24 22:28:58 +0100

    chore(seeders): Add categroy, product, reviews seeders and factories with sample data

--- backend/database/seeders/ReviewSeeder.php
+++ backend/database/seeders/ReviewSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use App\Models\Product;
 use App\Models\Review;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
 
